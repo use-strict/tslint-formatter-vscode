3fe3764b138bccf18407c19ecedacbe58af60e26
Alexandru Ciuca <gmail.rulz@yahoo.com>
2016-09-10 09:43:52 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+[![Build Status](https://travis-ci.org/use-strict/tslint-formatter-vscode.svg?branch=master)](https://travis-ci.org/use-strict/tslint-formatter-vscode)
+[![npm version](https://badge.fury.io/js/tslint-formatter-vscode.svg)](https://badge.fury.io/js/tslint-formatter-vscode)
+
+# tslint-formatter-vscode
+
+A TSLint formatter for Visual Studio Code
+
+## Format
+
+```
+$file($line,$col): warning TSLINT: $message ($ruleName)
+$file($line,$col): error TSLINT: $message ($ruleName)
+```
---
+++ src/vscodeWarningFormatter.ts
@@ -0,0 +1,21 @@
+import * as Lint from "tslint/lib/lint";
+
+export class Formatter extends Lint.Formatters.AbstractFormatter {
+    public format(failures: Lint.RuleFailure[]) {
+        let outputLines = failures.map(this.formatFailure);
+
+        return outputLines.join("\n") + "\n";
+    }
+
+    private formatFailure(failure: Lint.RuleFailure) {
+        let fileName = failure.getFileName();
+        let message = failure.getFailure();
+        let ruleName = failure.getRuleName();
+
+        let lineAndCharacter = failure.getStartPosition().getLineAndCharacter();
+        let positionTuple = (lineAndCharacter.line + 1) + ',' +
+            (lineAndCharacter.character + 1);
+
+        return `${fileName} (${positionTuple}): warning TSLINT:${message} (${ruleName})`;
+    }
+}
